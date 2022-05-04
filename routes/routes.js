9bda5c3cf23155229789e58b3822e1893efa6fbc
theonlyrichard <richard.cardoza16@gmail.com>
2022-05-03 20:15:10 -0700

    fix errors

--- routes/routes.js
+++ routes/routes.js
@@ -2,5 +2,38 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = app => {
+    fs.readFile("db/db.json", "utf8", (err, data) => {
 
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+        app.get("/api/notes", function (req, res) {
+            res.json(notes);
+        });
+
+        app.post("/api/notes", function (req, res) {
+            // Receives a new note, adds it to db.json, then returns the new note
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            return console.log("Added new note: " + newNote.title);
+        });
+
+
+        app.get('/notes', function (req, res) {
+            res.sendFile(path.join(__dirname, "../public/notes.html"));
+        });
+        app.get('*', function (req, res) {
+            res.sendFile(path.join(__dirname, "../public/index.html"));
+        });
+
+        //updates the json file 
+        function updateDb() {
+            fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+                if (err) throw err;
+                return true;
+            });
+        }
+
+    });
 }
--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-// route file requirements
-require('./routes/routes')(app);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+//Require routes file
+require('./routes/routes')(app);
 
 // listener
 app.listen(PORT, function () {
